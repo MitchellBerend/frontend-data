4e32fadd7001e07a63f57dc96889879e45e1394a
Mitchell Berendhuysen <45570310+MitchellBerend@users.noreply.github.com>
2024-04-03 13:43:57 +0200

    Added initial logging to check for init process (#13)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"frontend-data/internal/server"
+	"log"
+	"os"
 )
 
 func main() {
 	server := server.NewServer()
 
+	log.Printf("Listening on 0.0.0.0:%s\n", os.Getenv("PORT"))
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
