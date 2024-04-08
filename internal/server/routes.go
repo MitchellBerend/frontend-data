e99942b002d277c0f40db2764f772298455549bb
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2024-04-08 21:12:12 +0200

    Updated cors

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -17,7 +17,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*.berendhuysen.nl", "http://localhost"},
+		AllowedOrigins:   []string{"https://*.berendhuysen.nl", "http://*"},
 		AllowedMethods:   []string{"GET", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
